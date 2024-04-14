de7bad3e3454740e4c36c41b69dc54fad90786a9
Fadi <fadinachawati17@gmail.com>
2024-04-14 20:24:32 +0200

    Features component

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,8 @@
+import {ScrollTrigger} from "gsap/all";
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger);
+
 export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
     timeline.to(rotationRef.current.rotation, {
         y: rotationState,
@@ -23,4 +28,17 @@ export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, fi
         },
         '<'
     )
+}
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+    gsap.to(target, {
+        ...animationProps,
+        scrollTrigger: {
+            trigger: target,
+            // toggleActions: enter, leave, enter back, leave back
+            toggleActions: "restart reverse restart reverse",
+            start: "top 85%",
+            ...scrollProps
+        }
+    })
 }
